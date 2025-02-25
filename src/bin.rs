f1f8abc3a54e75ef3cb885b01d1d142549146de4
Jason Yu <jasonyu.kr@gmail.com>
2025-02-25 18:08:00 +0900

    Customize output

--- src/bin.rs
+++ src/bin.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
+use colored::Colorize;
 
 use lscolors::{LsColors, Style};
 
@@ -16,7 +17,7 @@ compile_error!(
     "one feature must be enabled: ansi_term, nu-ansi-term, crossterm, gnu_legacy, owo-colors"
 );
 
-fn print_path(handle: &mut dyn Write, ls_colors: &LsColors, path: &str) -> io::Result<()> {
+fn print_path(handle: &mut dyn Write, ls_colors: &LsColors, path: &str, trailing_slash: bool) -> io::Result<()> {
     for (component, style) in ls_colors.style_for_path_components(Path::new(path)) {
         #[cfg(any(feature = "nu-ansi-term", feature = "gnu_legacy"))]
         {
@@ -42,6 +43,9 @@ fn print_path(handle: &mut dyn Write, ls_colors: &LsColors, path: &str) -> io::R
             write!(handle, "{}", component.to_string_lossy().style(ansi_style))?;
         }
     }
+    if trailing_slash {
+        write!(handle, "/")?;
+    }
     writeln!(handle)?;
 
     Ok(())
@@ -60,7 +64,7 @@ fn run() -> io::Result<()> {
         args.next();
 
         for arg in args {
-            print_path(&mut stdout, &ls_colors, &arg)?;
+            print_path(&mut stdout, &ls_colors, &arg, false)?;
         }
     } else {
         let stdin = io::stdin();
@@ -74,7 +78,14 @@ fn run() -> io::Result<()> {
             let path_str = String::from_utf8_lossy(&buf[..(buf.len() - 1)]);
             #[cfg(windows)]
             let path_str = path_str.trim_end_matches('\r');
-            print_path(&mut stdout, &ls_colors, path_str.as_ref())?;
+
+            let path = Path::new(path_str.as_ref());
+            if !path.exists() {
+                // Print whole red line for the path-not-found case
+                writeln!(stdout, "{}", path_str.red())?;
+            } else {
+                print_path(&mut stdout, &ls_colors, path_str.as_ref(), path.is_dir())?;
+            }
 
             buf.clear();
         }
